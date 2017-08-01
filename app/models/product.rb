7256e923df1841ec44c4852eb471fe053f49ad1f
Akshit Sahani <akshit_sahani@hotmail.com>
2017-08-01 17:11:46 -0400

    predict size method partially done. Complete it

--- app/models/product.rb
+++ app/models/product.rb
@@ -68,4 +68,48 @@ class Product < ApplicationRecord
     return final_desc.join(' ')
   end
 
+  def get_predicted_storesize(user)
+    predicted_hip = user.predicted_hip
+    predicted_waist = user.predicted_waist
+    predicted_bust = user.predicted_bust
+
+    results_bust = Store.where(store_name: "VOLUPTUOUS", feature: 'BUST')
+    results_waist = Store.where(store_name: "VOLUPTUOUS", feature: 'WAIST')
+    results_hip = Store.where(store_name: "VOLUPTUOUS", feature: 'HIP')
+
+    results_hash = {}
+
+    results_bust.each do |result|
+      if predicted_bust <= result.size_max.to_f && predicted_bust >= result.size_min.to_f
+        results_hash[:bust] = result.store_size
+      end
+    end
+
+    results_waist.each do |result|
+      if predicted_waist <= result.size_max.to_f && predicted_waist >= result.size_min.to_f
+        results_hash[:waist] = result.store_size
+      end
+    end
+
+    results_hip.each do |result|
+      if predicted_hip <= result.size_max.to_f && predicted_hip >= result.size_min.to_f
+         results_hash[:hip] = result.store_size
+      end
+    end
+
+    if self.dresses != nil
+      #split by or and then by / and compare the numbers for each and select largest size.
+    end
+
+    if self.tops != nil
+      #split by or and then by / and compare the numbers for bust and waist and select largest size.
+    end
+
+    if self.bottoms != nil
+      #split by or and then by / and compare the numbers for waist and hip and select largest size.
+    end
+
+    return results_hash
+  end
+
 end
