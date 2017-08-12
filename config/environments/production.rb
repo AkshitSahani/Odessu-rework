0604104ed7dccc81f425b34ebf6b6a9c0fb6e5bc
Akshit Sahani <akshit_sahani@hotmail.com>
2017-08-12 19:31:14 -0400

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   enable_starttls_auto: true,
   user_name: ENV['GMAIL_USERNAME'],
   password: ENV['GMAIL_PASSWORD']
-
+}
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
